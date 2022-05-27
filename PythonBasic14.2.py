dcff647dbc391ca9d3f4a82c9becd07f3fcd58ab
Nellulushka <nelia.shopping@gmail.com>
2022-05-27 13:22:42 -0400

    add new code

--- PythonBasic14.2.py
+++ PythonBasic14.2.py
@@ -1,3 +1,8 @@
 my_list = [10, 50, 20, 25, 30]
 sliced_list = my_list[1:3]
 print(sliced_list)
+my_string = "Test Automation"
+word_test = my_string[0:4]
+print(word_test)
+second_word = my_string[5:15]
+print(second_word)
